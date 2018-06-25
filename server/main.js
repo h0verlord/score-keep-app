dc50947babaa3f20c10c2946d2b455ce65a59f08
rzevel <richard.zevel@gmail.com>
2018-06-25 15:02:31 +0200

    practice es6 classes
    constructors, inheritance, accessing methods and members

--- server/main.js
+++ server/main.js
@@ -6,5 +6,56 @@ import {
 } from '../imports/api/players'
 
 Meteor.startup(() => {
-      
+      class Person {
+            constructor(name = 'Anonymous', age = 0) {
+                  this.name = name
+                  this.age = age
+            }
+            getPersonDescription() {
+                  return `${this.name} is ${this.age} year(s) old.`
+            }
+            getGreeting() {
+                  return `Hi, I am ${this.name}.`
+            }
+      }
+
+      class Employee extends Person {
+            constructor(name, age, title) {
+                  super(name, age)
+                  this.title = title
+            }
+            hasJob() {
+                  return !!this.title;
+            }
+            getGreeting() {
+                  if (this.title) {
+                        return `Hi, I am ${this.name} and I work as a ${this.title}`
+                  } else {
+                        return super.getGreeting()
+                  }
+            }
+      }
+
+      class Programmer extends Person{
+            constructor(name, age, usedLanguage = 'assembly'){
+                  super(name, age)
+                  this.usedLanguage = usedLanguage
+            }
+
+            getGreeting(){
+                  if(this.usedLanguage){
+                        return `Hi, I am ${this.name}. I am a ${this.usedLanguage} developer.`
+                  } else{
+                        return super.getGreeting()
+                  }
+            }
+      }
+
+      let js = new Programmer('John', 26, 'JS')
+      console.log(js.getGreeting())
+
+      let def = new Programmer('Peter', 23)
+      console.log(def.getGreeting())
+
+
 })
