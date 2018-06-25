7596aaa50f241489f5924887180389ebca477957
rzevel <richard.zevel@gmail.com>
2018-06-25 16:50:22 +0200

    TitleBar and AddPlayer component structure

--- client/main.js
+++ client/main.js
@@ -3,6 +3,8 @@ import ReactDOM from "react-dom";
 import { Meteor } from "meteor/meteor";
 import {Tracker} from 'meteor/tracker'
 import {Players} from './../imports/api/players'
+import TitleBar from './../imports/ui/TitleBar'
+import AddPlayer from './../imports/ui/AddPlayer'
 
 
 const renderPlayers = (players) => {
@@ -23,32 +25,16 @@ const renderPlayers = (players) => {
       });
     };
 
-    const handleSubmit= (e) => {
-      let playerName = e.target.playerName.value
-      e.preventDefault()
-      if (playerName){
-            e.target.playerName.value = ''
-            Players.insert({
-                  name: playerName,
-                  score: 0
-            })
-      }
-    }
-
 Meteor.startup(() => {
       Tracker.autorun(()=>{
            let players = Players.find().fetch()
-           let title = "Score Keep";
-           let name = "Richard";
+           let title = "Score Keep"
+           let subTitle = ''
            let jsx = (
              <div >
-               <h1>{title}</h1>
-               <p>Hello {name}</p>
+               <TitleBar title={title} subTitle = 'Created by Me'/>
                {renderPlayers(players)}
-               <form onSubmit={handleSubmit}>
-                  <input type="text" name="playerName" placeholder="Player name"/>
-                  <button>Add player</button>
-               </form>
+               <AddPlayer />
              </div>
            );
            ReactDOM.render(jsx, document.getElementById("app"));
