fee0fd6ce607b01406f5b5f8dbfd4f990bdc6a16
rzevel <richard.zevel@gmail.com>
2018-06-25 17:01:57 +0200

    started implementing listing of Players in a special Component

--- client/main.js
+++ client/main.js
@@ -5,25 +5,21 @@ import {Tracker} from 'meteor/tracker'
 import {Players} from './../imports/api/players'
 import TitleBar from './../imports/ui/TitleBar'
 import AddPlayer from './../imports/ui/AddPlayer'
+import Player from './../imports/ui/Player'
 
 
 const renderPlayers = (players) => {
       return players.map((player) => {
-        return (<p key={player._id}>
-            {player.name}: {player.score} points.
-
-            <button onClick={() => {
-                  Players.update(player._id, { $inc: {score: 1}
-            })}}>+1</button>
-
-            <button onClick={() => {
-                  Players.update(player._id, { $inc: {score: -1}
-            })}}>-1</button>
-
-            <button onClick={()=> Players.remove(player._id)}>X</button>
-      </p>);
-      });
-    };
+            return <Player key={player._id}/>
+      
+            //   return (<p key={player._id}>
+      //       {player.name}: {player.score} points.
+      //       <button onClick={() => { Players.update(player._id, { $inc: {score: 1}})}}>+1</button>
+      //       <button onClick={() => { Players.update(player._id, { $inc: {score: -1}})}}>-1</button>
+      //       <button onClick={()=> Players.remove(player._id)}>X</button>
+      // </p>)
+      })
+    }
 
 Meteor.startup(() => {
       Tracker.autorun(()=>{
---
+++ imports/ui/Player.js
@@ -0,0 +1,8 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+export default class Player extends React.Component{
+      render(){
+            return <p>Dummy text for player</p>
+      }
+}
