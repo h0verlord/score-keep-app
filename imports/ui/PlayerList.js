2e8945e9047ec42a840f87c7d71c6ff19c2561d3
rzevel <richard.zevel@gmail.com>
2018-06-27 16:05:32 +0200

    Condition rendering when there are no players implemented

--- imports/ui/PlayerList.js
+++ imports/ui/PlayerList.js
@@ -4,9 +4,15 @@ import PropTypes from 'prop-types'
 
 export default class PlayerList extends React.Component{
       renderPlayers(){
-            return this.props.players.map((player) => {
-                  return <Player key={player._id} id={player._id} score={player.score} name={player.name}/>
-            })
+            if(this.props.players.length === 0){
+                  return (
+                        <p>Add your first player to get started</p>
+                  )
+            } else{
+                  return this.props.players.map((player) => {
+                        return <Player key={player._id} id={player._id} score={player.score} name={player.name}/>
+                  })
+            }
       }
       render(){
             return (
